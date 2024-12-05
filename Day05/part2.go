82ef148d29b00fb3bc60cdb94bb4267651b12495
HannesNowak <62110797+HannesNowak@users.noreply.github.com>
2024-12-05 18:57:36 +0100

    Day 05 Part 02

---
+++ Day05/part2.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"time"
+
+	utils "github.com/HannesNowak/AdventOfCode/utils"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	lines := utils.ReadLines(scanner)
+	var res int
+	startTime := time.Now()
+
+	rules := map[int][]int{}
+	for _, line := range lines {
+		nums := utils.AllInts(line)
+		rules[nums[0]] = append(rules[nums[0]], nums[1])
+	}
+
+	updates := utils.ReadLines(scanner)
+
+	for _, update := range updates {
+		order := utils.AllInts(update)
+		if !swapUpdate(order, rules) {
+			continue
+		}
+		eval := evalUpdate(order, rules)
+		res += eval
+	}
+
+	fmt.Println("Execution time:", time.Since(startTime))
+	fmt.Println(res)
+}
+
+func swapUpdate(update []int, rules map[int][]int) bool {
+	reordered := false
+	for idx, page := range update {
+		for i := 0; i < idx; i++ {
+			if slices.Contains(rules[page], update[i]) {
+				reordered = true
+				update[idx] = update[idx] ^ update[i]
+				update[i] = update[i] ^ update[idx]
+				update[idx] = update[idx] ^ update[i]
+			}
+		}
+	}
+	return reordered
+}
+
+func evalUpdate(update []int, rules map[int][]int) int {
+	for idx, page := range update {
+		for i := 0; i < idx; i++ {
+			if slices.Contains(rules[page], update[i]) {
+				return 0
+			}
+		}
+	}
+	return update[len(update)/2]
+}
